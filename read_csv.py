fc7713b9af865fd3e292c2329a00998ccf121e75
juliarmn <julia.rm.nascimento@unesp.br>
2023-03-13 22:33:53 -0300

    Arruma csv

--- read_csv.py
+++ read_csv.py
@@ -1,6 +1,16 @@
 import pandas as pd
-#Lembre que deve estar na mesma pasta
-dataframe = pd.read_csv('jua.csv')
+#Lembre que deve estar na mesma pasta -> senão passar caminho
+dataframe = pd.read_csv('jua.csv', sep=';')
 print(dataframe)
 
 #Código simples para pegar informações de arquivos csv
+#Se for passar caminho, colocar r na frente (raw_string)
+
+#Colunas como chave de dicionário
+print('\n\n')
+print(dataframe['cor'])
+
+#Até uma linha apenas:
+#Linhas são como listas
+print('\n\n')
+print(dataframe[:3])
